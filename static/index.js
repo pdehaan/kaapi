a8285d4ccfcc48f7afcbb0e170afd37540ed9517
Aditya <aditya@netroy.in>
2014-01-07 18:28:17 +0100

    update

--- static/index.js
+++ static/index.js
@@ -2,8 +2,9 @@
 
   'use strict';
 
-  var document = global.document;
+  // var document = global.document;
   var mocha = global.mocha;
+  var requirejs = global.requirejs;
 
   function ajax (url, callback) {
     var xhr = new XMLHttpRequest();
@@ -18,16 +19,6 @@
     xhr.send();
   }
 
-  var parent = document.getElementsByTagName('script')[0].parentNode;
-  function newScript (source) {
-    var script = document.createElement('script');
-    script.language = 'javascript';
-    script.type = 'text/javascript';
-    script.defer = true;
-    script.text = source;
-    parent.appendChild(script);
-  }
-
   ajax('/config', function (config) {
     config = JSON.parse(config);
 
@@ -40,24 +31,13 @@
       mocha.grep(config.grep);
     }
 
+    global.isNode = false;
     global.should = global.chai.should();
     global.expect = global.chai.expect;
 
-    var files = config.files;
-
-    function next () {
-      var file = files.shift();
-      ajax(file, function (source) {
-        newScript(source);
-        if (files.length) {
-          next();
-        } else {
-          mocha.run();
-        }
-      });
-    }
-
-    next();
+    requirejs(config.files, function () {
+      mocha.run();
+    });
   });
 
 })(this);
